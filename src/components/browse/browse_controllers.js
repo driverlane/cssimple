357435ef20cea9ad579d4b8645f2e44f306cf643
unknown <mark.farrall@content.cgi.demo>
2014-09-30 09:24:47 +1000

    Initial version

--- src/components/browse/browse_controllers.js
+++ src/components/browse/browse_controllers.js
@@ -7,22 +7,31 @@
 angular.module('browse').controller('BrowseController', function($scope, $routeParams, $rootScope, $location, csRepository) {
 
 	// see if we've got an authenticated connection
-	if (!csRepository.authenticated) {
+	if (!csRepository.initialised) {
 		// todo authenticate
 		
 	}
+	
+	
 
-	// get the container node
-	var thisnode = $routeParams.id;
-	if (!thisnode) {
-		thisnode = $rootScope.startNode;
+	// get the container id
+	$scope.containerId = $routeParams.id;
+	if (!$scope.containerId) {
+		$scope.containerId = $rootScope.startNode;
 	}
 
 	// get the details for the current container
-	$scope.container = csRepository.getNode(thisnode);
+	csRepository.getNode($scope.containerId)
+	.then(function(data) {
+		$scope.container = data;
+	});
 	
 	// get the nodes for the current container
-	$scope.nodes = csRepository.getChildren(thisnode).data;
+	//$scope.nodes = csRepository.getChildren($scope.containerId).data;
+	csRepository.getChildren($scope.containerId)
+	.then(function(data) {
+		$scope.nodes = data.data;
+	});
 
 	// handles the click event for nodes
 	$scope.openNode = function(type, id) {
--- src/components/browse/browse_directives.js
+++ src/components/browse/browse_directives.js
@@ -10,36 +10,40 @@ angular.module('browse').directive('objectIcon', function() {
 		restrict: 'A',
 		replace: 'false',
 		link: function(scope, el, atts) {
-			var args = atts.objectIcon.split(',');
+			scope.$watch('container', function() {
 
-			// append the object icon class
-			var classes = '';
-			switch(args[0]) {
-				case '141':
-					classes = 'fa-home';
-					break;
-				case '0':
-					classes = 'fa-folder-open';
-					break;
-				case '144':
-					classes = 'fa-file-pdf-o';
-					break;
-				default:
-					classes = 'fa-bars';
-					break;
-			}
-			
-			// combine the classes
-			if (args[1] == 'browse') {
-				classes = 'browse-icon fa ' + classes  + ' fa-2x';
-			}
-			else {
-				classes = 'browse-container-name-icon fa ' + classes  + ' fa-3x';
-			}
-			
-			// update the attributes
-			el.attr('class', classes);
-			el.removeAttr('browse-icon');
+				var args = atts.objectIcon.split(',');
+
+				// append the object icon class
+				var classes = '';
+				switch(args[0]) {
+					case '141':
+						classes = 'fa-home';
+						break;
+					case '0':
+						classes = 'fa-folder-open';
+						break;
+					case '144':
+						classes = 'fa-file-pdf-o';
+						break;
+					default:
+						classes = 'fa-bars';
+						break;
+				}
+				
+				// combine the classes
+				if (args[1] == 'browse') {
+					classes = 'browse-icon fa ' + classes  + ' fa-2x';
+				}
+				else {
+					classes = 'browse-container-name-icon fa ' + classes  + ' fa-3x';
+				}
+				
+				// update the attributes
+				el.attr('class', classes);
+				el.removeAttr('browse-icon');
+
+			});
 		}
 	};
 
